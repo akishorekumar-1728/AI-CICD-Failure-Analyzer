89c1e63db78ea11439500ac8b1753e48b70e0ad5
akishorekumar-1728 <akishorekumar1728@gmail.com>
2026-06-29 11:23:04 +0530

    Updated Flask dashboard UI

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+builds = []
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", builds=builds)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+
+    print("Webhook Received")
+    print(data)
+
+    builds.append({
+        "message": "Webhook received successfully"
+    })
+
+    return {"status": "success"}, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
